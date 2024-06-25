400a67d04e894b4b5ad6dfdcec6b49c96b3a993b
MM <gmax3000@gmail.com>
2024-06-24 20:45:05 -0700

    dependency prop-types

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const Home = () => <h2>Home Page</h2>;
 const About = () => <h2>About Page</h2>;
 const Contact = () => <h2>Contact Page</h2>;
 
+// Define prop types for the Home component
+Home.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
+      <Route exact path="/" render={() => <Home message="Welcome to the Home Page!" />} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
       </Switch>
@@ -18,3 +25,5 @@ const App = () => {
 };
 
 export default App;
+
+
